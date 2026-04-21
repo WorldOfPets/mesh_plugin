88ac9b3a2c75aa160fe42829415a39a2e8b7275a
Evgen Polivanov <polivanov004@gmail.com>
2026-04-21 12:02:15 +0300

    final version with sync KTP

--- content.js
+++ content.js
@@ -1,3 +1,11 @@
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null;
+}
+
+
 function sendLog(message) {
   chrome.runtime.sendMessage({ type: 'log', message: message });
 }
